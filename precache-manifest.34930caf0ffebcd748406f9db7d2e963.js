c33618c059e412bb25ca1aedb144c9a2fddd02c6
Devon Church <notifydevon@gmail.com>
2020-04-13 14:52:29 +1200

    Updates

---
+++ precache-manifest.34930caf0ffebcd748406f9db7d2e963.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2123585e8223afb79074e9225f42d4f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93f5b45fed13c6a28905",
+    "url": "/static/js/2.54a7827c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.54a7827c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "251da1bc0cdc1aac43af",
+    "url": "/static/js/main.1d1b99cf.chunk.js"
+  },
+  {
+    "revision": "9a65a2ef2ec1a55cf1e9",
+    "url": "/static/js/runtime-main.03d7eba3.js"
+  }
+]);
